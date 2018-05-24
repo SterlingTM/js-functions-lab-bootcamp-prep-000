1a326fb41656e1d81de1ec9dabd254c7da6b0125
Sterling Moss <mtsilvertm@gmail.com>
2018-05-24 16:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ function happyHolidays(string) {
   return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
+<<<<<<< HEAD
  return `Happy holidays, ${name}!`;
 }
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
+=======
+ return `Happy Holidays, ${name}!`;
+}
+function happyHolidayTo(holiday, name) {
+  return `${happy holiday}, ${name}!`
+>>>>>>> 8755d6a429dd845f4bba59a3d0369f90e2403a8b
 }
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
